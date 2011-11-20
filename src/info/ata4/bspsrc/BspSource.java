cc57ee43fe5b79cd071670d2930d31a1830449da
BarracudaATA <barracuda415@yahoo.de>
2011-11-21 00:48:32 +0100

    Changed version to 1.3.1 because of BSPLib update

--- src/info/ata4/bspsrc/BspSource.java
+++ src/info/ata4/bspsrc/BspSource.java
@@ -35,7 +35,7 @@ public class BspSource implements Runnable {
     
     private static final Logger L = Logger.getLogger(BspSource.class.getName());
 
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.3.1";
     
     private final BspSourceConfig config;
     
