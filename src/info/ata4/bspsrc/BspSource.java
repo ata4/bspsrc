290fa8fd30d8c1a427751d89efa24d80ed3ba20a
ata4 <barracuda415@yahoo.de>
2013-02-20 15:37:14 +0100

    Incremented patch version

--- src/info/ata4/bspsrc/BspSource.java
+++ src/info/ata4/bspsrc/BspSource.java
@@ -35,7 +35,7 @@ public class BspSource implements Runnable {
     
     private static final Logger L = Logger.getLogger(BspSource.class.getName());
 
-    public static final String VERSION = "1.3.9";
+    public static final String VERSION = "1.3.10";
     
     private final BspSourceConfig config;
     
