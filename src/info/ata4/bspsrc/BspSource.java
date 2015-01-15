031926e4a73b2f31ee7e1c6560be9703fef0dabf
ata4 <barracuda415@yahoo.de>
2015-01-15 18:50:38 +0100

    Updated version

--- src/info/ata4/bspsrc/BspSource.java
+++ src/info/ata4/bspsrc/BspSource.java
@@ -35,7 +35,7 @@ public class BspSource implements Runnable {
     
     private static final Logger L = LogUtils.getLogger();
 
-    public static final String VERSION = "1.3.18";
+    public static final String VERSION = "1.3.19";
     
     private final BspSourceConfig config;
     
