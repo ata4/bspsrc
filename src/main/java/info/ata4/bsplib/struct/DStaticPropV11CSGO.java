ddc92cf9979b3e9f39a625e0ecfa963edce7071d
GitRihi <hendrik@mika.gl>
2018-08-08 03:35:50 +0200

    Added fallback option for CS:GO maps created prior to the 'uniformscale' update

--- src/main/java/info/ata4/bsplib/struct/DStaticPropV11CSGO.java
+++ src/main/java/info/ata4/bsplib/struct/DStaticPropV11CSGO.java
@@ -1,44 +1,30 @@
-/*
- ** 2012 April 24
- **
- ** The author disclaims copyright to this source code.  In place of
- ** a legal notice, here is a blessing:
- **    May you do good and not evil.
- **    May you find forgiveness for yourself and forgive others.
- **    May you share freely, never taking more than you give.
- */
 package info.ata4.bsplib.struct;
 
 import info.ata4.io.DataReader;
 import info.ata4.io.DataWriter;
+
 import java.io.IOException;
 
-/**
- * Old V10 structure found in CS:GO.
- * 
- * @author Nico Bergemann <barracuda415 at yahoo.de>
- */
-public class DStaticPropV11CSGO extends DStaticPropV9 {     //CS:GO now uses v11  since the addition of uniform prop scaling
+//CS:GO now uses v11  since the addition of uniform prop scaling
+public class DStaticPropV11CSGO extends DStaticPropV10CSGO {
 
-    protected int unknown;
     public float uniformScale;
 
     @Override
     public int getSize() {
-        return super.getSize() + 4 + 4; // 80
+        return super.getSize() + 4; // 80
     }
 
     @Override
-    public void read(DataReader in) throws IOException {
+    public void read(DataReader in) throws IOException
+    {
         super.read(in);
-        unknown = in.readInt();
         uniformScale = in.readFloat();
     }
 
     @Override
     public void write(DataWriter out) throws IOException {
         super.write(out);
-        out.writeInt(unknown);
         out.writeFloat(uniformScale);
     }
 }
