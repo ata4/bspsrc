346c663301e5a5227546ad261bb13080bf026b3e
gitRihi <19492038+gitRihi@users.noreply.github.com>
2018-12-10 00:03:58 +0100

    Added support for drone clips

--- src/main/java/info/ata4/bspsrc/modules/texture/ToolTexture.java
+++ src/main/java/info/ata4/bspsrc/modules/texture/ToolTexture.java
@@ -30,7 +30,6 @@ public final class ToolTexture {
     public static final String CLIP = "tools/toolsclip";
     public static final String PLAYERCLIP = "tools/toolsplayerclip";
     public static final String NPCCLIP = "tools/toolsnpcclip";
-    public static final String GRENADECLIP = "tools/toolsgrenadeclip";
     public static final String AREAPORTAL = "tools/toolsareaportal";
     public static final String BLOCKLIGHT = "tools/toolsblocklight";
     public static final String BLOCKBULLETS = "tools/toolsblockbullets";
@@ -38,4 +37,8 @@ public final class ToolTexture {
     public static final String INVISLADDER = "tools/toolsinvisibleladder";
     public static final String DOTTED = "tools/toolsdotted";
     public static final String OCCLUDER = "tools/toolsoccluder";
+
+    //CSGO Only
+    public static final String CSGO_GRENADECLIP = "tools/toolsgrenadeclip";
+    public static final String CSGO_DRONECLIP = "tools/toolsdroneclip";
 }
