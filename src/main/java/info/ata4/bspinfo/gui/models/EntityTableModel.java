f0da5044a934344a8ecad960ba0841fd6c65f7a1
rihi <19492038+rihi@users.noreply.github.com>
2020-02-14 02:11:36 +0100

    Improve EntityTableModel

--- src/main/java/info/ata4/bspinfo/gui/models/EntityTableModel.java
+++ src/main/java/info/ata4/bspinfo/gui/models/EntityTableModel.java
@@ -12,7 +12,9 @@ package info.ata4.bspinfo.gui.models;
 import info.ata4.bsplib.BspFileReader;
 import info.ata4.bsplib.entity.Entity;
 import info.ata4.util.gui.ListTableModel;
-import java.util.*;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -22,29 +24,17 @@ public class EntityTableModel extends ListTableModel {
 
     public EntityTableModel() {
         super(2);
-        columnNames = Arrays.asList(new String[]{"Class", "Entities"});
+        columnNames = Arrays.asList("Class", "Entities");
         columnClasses = new Class[] {String.class, Integer.class};
     }
 
     public EntityTableModel(BspFileReader bspReader) {
         this();
 
-        Set<String> classes = bspReader.getEntityClassSet();
-        List<Entity> entities = bspReader.getData().entities;
-        List<String> entityStrings = new ArrayList<>();
-
-        // create non-unique list of all entity classes
-        for (Entity ent : entities) {
-            entityStrings.add(ent.getClassName());
-        }
-
-        // create rows and count occurrences of all unique entity classes
-        for (String cls : classes) {
-            List<Object> row = new ArrayList<>();
-            row.add(cls);
-            row.add(Collections.frequency(entityStrings, cls));
-            addRow(row);
-        }
+        bspReader.getData().entities.stream()
+                .map(Entity::getClassName)
+                .collect(Collectors.groupingBy(s -> s, Collectors.counting()))
+                .forEach((cls, count) -> addRow(Arrays.asList(cls, count)));
     }
 
     @Override
