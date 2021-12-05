27a92e4f7c2fbfbbcdcef672ab3a618c028a1c53
rihi <19492038+rihi@users.noreply.github.com>
2021-12-05 18:13:59 +0100

    Replace deprecated 'new NullOutputStream()' with 'NullOutputStream .NULL_OUTPUT_STREAM' 2

--- src/main/java/info/ata4/bspsrc/modules/BspChecksum.java
+++ src/main/java/info/ata4/bspsrc/modules/BspChecksum.java
@@ -12,13 +12,14 @@ package info.ata4.bspsrc.modules;
 import info.ata4.bsplib.BspFileReader;
 import info.ata4.bsplib.lump.Lump;
 import info.ata4.bsplib.lump.LumpType;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.output.NullOutputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.zip.CRC32;
 import java.util.zip.CheckedInputStream;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.io.output.NullOutputStream;
 
 /**
  * BSP checksum calculator based on Source's server map CRC check.
@@ -42,7 +43,7 @@ public class BspChecksum extends ModuleRead {
 
             try (InputStream in = new CheckedInputStream(lump.getInputStream(), crc)) {
                 // copy to /dev/null, we need the checksum only
-                IOUtils.copy(in, new NullOutputStream());
+                IOUtils.copy(in, NullOutputStream.NULL_OUTPUT_STREAM);
             }
         }
 
