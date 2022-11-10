0d1a9a4fdd342795a907fef4efb10685fed20ca7
rihi <19492038+rihi@users.noreply.github.com>
2022-11-10 15:16:30 +0100

    Change version to 1.4.4-SNAPSHOT

--- src/main/java/info/ata4/bspsrc/BspSource.java
+++ src/main/java/info/ata4/bspsrc/BspSource.java
@@ -41,7 +41,7 @@ public class BspSource implements Runnable {
 
     private static final Logger L = LogUtils.getLogger();
 
-    public static final String VERSION = "1.4.3";
+    public static final String VERSION = "1.4.4-DEV";
 
     private final BspSourceConfig config;
 
