dcd9b7af7cc4883e0a14ab56bb868beaebcf368a
rihi <19492038+rihi@users.noreply.github.com>
2023-09-02 02:21:27 +0200

    Bump version to 1.4.5-SNAPSHOT

--- bspsrc-decompiler/src/main/java/info/ata4/bspsrc/decompiler/BspSource.java
+++ bspsrc-decompiler/src/main/java/info/ata4/bspsrc/decompiler/BspSource.java
@@ -56,7 +56,7 @@ public class BspSource {
     private static final Logger L = LogManager.getLogger();
     public static final String DECOMPILE_TASK_ID_IDENTIFIER = "decompile_id";
 
-    public static final String VERSION = "1.4.4";
+    public static final String VERSION = "1.4.5-DEV";
 
     private final BspSourceConfig config;
     private final List<BspFileEntry> entries;
